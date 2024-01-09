4075a5941d50effdabc7f07297287e64db9d2109
Chenhui <chenhui.li@shopee.com>
2024-01-09 12:09:17 +0800

    add jump game

--- internal/demos/genaral/demos.go
+++ internal/demos/genaral/demos.go
@@ -1,6 +1,9 @@
 package genaral
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func fmtLibs() {
 	var (
@@ -10,4 +13,57 @@ func fmtLibs() {
 	)
 	fmt.Scanf("1:%s 2:%d 3:%t", &name, &age, &married)
 	fmt.Printf("扫描结果 name:%s age:%d married:%t \n", name, age, married)
+}
+
+// 定义结构体
+type User struct {
+	Id   int
+	Name string
+	Age  int
+}
+
+// 绑方法
+func (u User) Hello() {
+	fmt.Println("Hello")
+}
+
+func reflect12(obj interface{}) {
+	t := reflect.TypeOf(obj)
+	v := reflect.ValueOf(obj)
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		fmt.Printf("%s : %v", field.Name, field.Type)
+		val := v.Field(i).Interface()
+		fmt.Println("value: ", val)
+	}
+	fmt.Println("---------print functions-------")
+
+	for i := 0; i < t.NumMethod(); i++ {
+		m := t.Method(i)
+		fmt.Println(m.Name)
+		fmt.Println(m.Type)
+	}
+}
+
+// https://leetcode.com/problems/jump-game/description/?envType=study-plan-v2&envId=top-interview-150
+func canJump(nums []int) bool {
+	firstIndex := 0
+	return jumpForward(firstIndex, nums)
+}
+
+func jumpForward(currentIndex int, nums []int) bool {
+	lastIndex := len(nums) - 1
+	if currentIndex == lastIndex {
+		return true
+	} else if currentIndex < lastIndex {
+		currentNode := nums[currentIndex]
+		if currentNode == 0 {
+			return false
+		}
+		return jumpForward(currentIndex+currentNode, nums)
+	} else {
+		// out of index
+		return false
+	}
 }
--- internal/demos/genaral/demos_test.go
+++ internal/demos/genaral/demos_test.go
@@ -1,7 +1,21 @@
 package genaral
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestFmtLibs(t *testing.T) {
 	fmtLibs()
+}
+
+func TestReflect(t *testing.T) {
+	obj := &User{1, "Li", 20}
+	reflect12(*obj)
+}
+
+func TestCanJump(t *testing.T) {
+	//nums := []int{2, 3, 1, 1, 4}
+	nums := []int{3, 2, 1, 0, 4}
+	fmt.Println(canJump(nums))
 }
